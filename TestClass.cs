de83b9e77234b76bf53ef7cd19542e5c28ee7e63
Guillaume Téchené <guillaume.techene@outlook.com>
2025-01-25 16:11:10 +0100

    Initial commit

---
+++ TestClass.cs
@@ -0,0 +1,19 @@
+﻿namespace VerifyThousandSeparator;
+
+public class TestClass
+{
+    private readonly VerifySettings _verifySettings = new();
+
+    public TestClass()
+    {
+        _verifySettings.UseDirectory("Snapshots");
+    }
+
+    [Test]
+    [SetCulture("fr-FR")]
+    public async Task Verify_should_not_fail_when_checking_a_number_formatted_as_C0_in_FR_culture()
+    {
+        const decimal input = 1000.31m;
+        await Verify($"{input:C0}", _verifySettings);
+    }
+}
