00c3be05e749636d1dcd040a4be4f9dd4c2dad6f
Guillaume Téchené <guillaume.techene@outlook.com>
2025-01-25 17:08:54 +0100

    Remove Verify dependencies

--- TestClass.cs
+++ TestClass.cs
@@ -1,22 +1,9 @@
-﻿namespace VerifyThousandSeparator;
+﻿using NUnit.Framework;
+
+namespace VerifyThousandSeparator;
 
 public class TestClass
 {
-    private readonly VerifySettings _verifySettings = new();
-
-    public TestClass()
-    {
-        _verifySettings.UseDirectory("Snapshots");
-    }
-
-    [Test]
-    [SetCulture("fr-FR")]
-    public async Task Verify_should_not_fail_when_checking_a_number_formatted_as_C0_in_FR_culture()
-    {
-        const decimal input = 1000.31m;
-        await Verify($"{input:C0}", _verifySettings);
-    }
-
     [Test]
     [SetCulture("fr-FR")]
     public void Should_not_fail_with_basic_assert()
