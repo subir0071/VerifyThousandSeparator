c0a24c8dfaaf2f698acde99f85099bf677a38b82
Subir Ghosh <subir0071@users.noreply.github.com>
2025-01-28 11:46:34 -0700

    Update TestClass.cs

--- TestClass.cs
+++ TestClass.cs
@@ -1,20 +1,14 @@
-﻿using NUnit.Framework;
-using System;
-using System.Globalization;
-using System.Linq;
+using NUnit.Framework;
+
 namespace VerifyThousandSeparator;
 
 public class TestClass
 {
     [Test]
-   // [SetCulture("fr-FR")]
+    [SetCulture("fr-FR")]
     public void Should_not_fail_with_basic_assert()
     {
         const decimal input = 1000.31m;
-        Assert.That("{input:C0}", Is.EqualTo("1\u202F000 €"));
-
-       
+        Assert.That($"{input:C0}", Is.EqualTo("1\u202f000 €"));
     }
-    
 }
-
