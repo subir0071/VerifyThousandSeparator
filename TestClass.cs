7ecab3dfeb5372b66c2a98c796f645175f377b37
Subir Ghosh <subir0071@users.noreply.github.com>
2025-01-28 10:58:37 -0700

    Update TestClass.cs

--- TestClass.cs
+++ TestClass.cs
@@ -8,7 +8,16 @@ public class TestClass
     [SetCulture("fr-FR")]
     public void Should_not_fail_with_basic_assert()
     {
+       // const decimal input = 1000.31m;
+       // Assert.That($"{input:C0}", Is.EqualTo("1\u202F000 €"));
+
+         // Arrange
         const decimal input = 1000.31m;
-        Assert.That($"{input:C0}", Is.EqualTo("1\u202F000 €"));
+        
+        // Act
+        string formattedValue = string.Format(CultureInfo.GetCultureInfo("fr-FR"), "{0:C0}", input);
+
+        // Assert
+        Assert.That(formattedValue, Is.EqualTo("1\u202F000 €"));
     }
 }
